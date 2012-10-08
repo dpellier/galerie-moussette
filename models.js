3a36d56db619067b0d777e6f63a43f26062899ef
sfeir <sfeir@PHP03.(none)>
2012-10-08 15:56:27 +0200

    add production config for mongodb

--- models.js
+++ models.js
@@ -1,8 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-db = mongoose.connect('mongodb://localhost/theGallery');
-	
+switch(process.env.NODE_ENV) {
+    case 'production':
+        db = mongoose.connect('mongodb://admin:Mdp19735@ds039027.mongolab.com:39027/heroku_app8244270');
+        break;
+    default:
+    	db = mongoose.connect('mongodb://localhost/theGallery');
+    	break;
+}
+
 var Image = db.model('Image', new Schema({
 	'type': {type: String, index: true},
 	'data': String,
