571089e8afce653c309bc2c731a60f6f761a7a90
dpellier <dpellier@dpellier-HP-EliteBook-8560p.(none)>
2013-05-21 16:07:27 +0200

    Files deleted

--- routes/arts.js
+++
@@ -1,9 +0,0 @@
-var Image = require('../models').Image;
-
-exports.list = function(req, res) {
-	var type = req.originalUrl.substring(1);
-	
-	Image.find({type: type}, 'data', function(err, documents) {
-		res.render('arts', {images: documents});
-	});
-};
