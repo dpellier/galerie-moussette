f24e62551c6109cb015e24d396613ce3b220186d
dpellier <pellier.d@sfeir.com>
2013-06-10 14:40:56 +0200

    fix mongo connection

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.config = function() {
 	switch(process.env.NODE_ENV) {
 	    case 'production':
 	    	return {
-		    	databaseUri: 'mongodb://admin:motdepasse@ds039027.mongolab.com:39027/heroku_app8244270',
+		    	databaseUri: 'mongodb://admin:06cd924ee34d3918ec6c28ccb24623faba3b8e08@ds039027.mongolab.com:39027/heroku_app8244270',
 		    	smtp: 'smtp.mailgun.org',
 		    	mailLogin: 'postmaster@app8244270.mailgun.org',
 		    	mailPwd: '99rkun4xxjb9'
